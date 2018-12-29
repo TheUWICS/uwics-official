89cb822cb25976a16e052806e0d42769202a3723
Qarun Qadir Bissoondial <qaurn80@hotmail.com>
2018-12-29 02:42:00 -0400

    - Added 3 default themes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,15 @@ import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent implements OnDestroy {
+  dark = true;
+
+  theme_dict = {
+    'Dark': 'uwics-dark',
+    'Light': 'uwics-light',
+    'Pink': 'uwics-pink'
+  };
+
   mobileQuery: MediaQueryList;
-  content = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-       laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
 
   private _mobileQueryListener: () => void;
 
@@ -26,5 +29,23 @@ export class AppComponent implements OnDestroy {
   ngOnDestroy(): void {
     this.mobileQuery.removeListener(this._mobileQueryListener);
   }
+
+  switch_theme(event) {
+    const wrapper = document.querySelector('html');
+    console.log(event.target.innerText);
+    wrapper.classList.remove(wrapper.classList.item(0));
+    wrapper.classList.add(this.theme_dict[event.target.innerText]);
+  }
+
+  //   if (this.dark) {
+  //     wrapper.classList.add('uwics-light');
+  //     wrapper.classList.remove('uwics-dark');
+  //   } else {
+  //     wrapper.classList.add('uwics-dark');
+  //     wrapper.classList.remove('uwics-light');
+  //   }
+  //
+  //   this.dark = !this.dark;
+  // }
 
 }

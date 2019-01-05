8e5b2b8955d283c162bcf3845873bc35ae2e3a9c
Qarun Qadir Bissoondial <qaurn80@hotmail.com>
2019-01-05 16:52:43 -0400

    Added an interface to the about-card page to avoid compilation error when running ng build --prod

--- src/app/about-card/about-card.component.ts
+++ src/app/about-card/about-card.component.ts
@@ -1,12 +1,17 @@
 import {Component, Input, OnInit} from '@angular/core';
 
+export interface Info {
+  title: string;
+  text: string;
+}
+
 @Component({
   selector: 'app-about-card',
   templateUrl: './about-card.component.html',
   styleUrls: ['./about-card.component.scss']
 })
 export class AboutCardComponent implements OnInit {
-  @Input() content: Object;
+  @Input() content: Info;
   @Input() num: number;
 
   constructor() {

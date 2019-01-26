b3a0f995116aa3aebf327aa49c38a73b4fc7a648
Qarun Qadir Bissoondial <qaurn80@hotmail.com>
2019-01-26 15:53:06 -0400

    - Moved all light theme styles to uwics-light.scss - Fixed an issue with the page-header not changing with theme - Adjusted margin of header in the page-header to be appropriate size - Removed unnecessary code from project-page

--- src/app/project-page/project-page/project-page.component.ts
+++ src/app/project-page/project-page/project-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ProjectInfo} from '../../../model';
+import {HeaderText, ProjectInfo} from '../../../model';
 import {DataService} from '../../services/data.service';
 
 @Component({
@@ -8,9 +8,17 @@ import {DataService} from '../../services/data.service';
   styleUrls: ['./project-page.component.scss']
 })
 export class ProjectPageComponent implements OnInit {
+  header_text: HeaderText;
   approved: ProjectInfo[] = [];
 
-  constructor(private db: DataService) { }
+  constructor(private db: DataService) {
+    this.header_text = {
+      heading: 'Members\' Projects',
+      p_text: 'The Computing Society is filled with talented members eager to embark on their journey into the world of coding and\n' +
+        '    software development. Below are some of the projects that they have done, either when they were learning something\n' +
+        '    new or building a project of their own.'
+    };
+  }
 
   ngOnInit() {
     this.db.fetchData('accepted_projects').then((data: ProjectInfo[]) => {

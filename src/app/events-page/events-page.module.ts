e220b85f453915fc34e3b8f94f7edb91a2805ac5
Justin Ramjattan <justinramjattan@outlook.com>
2019-02-03 09:15:58 -0400

    - Started design of events page. - Created card to display events information. - Created list to display current and previous events. - Set card information to change based on item selected from list. - Populated events information array with more data.

--- src/app/events-page/events-page.module.ts
+++ src/app/events-page/events-page.module.ts
@@ -4,6 +4,9 @@ import { CommonModule } from '@angular/common';
 import { EventsPageRoutingModule } from './events-page-routing.module';
 import {EventsPageComponent} from './events-page/events-page.component';
 import {MatDividerModule} from '@angular/material/divider';
+import {MatCardModule} from '@angular/material/card'
+import {MatListModule} from '@angular/material/list'
+import {MatGridListModule} from '@angular/material/grid-list'
 import { SharedModule } from "../shared/shared.module";
 import { EventsInfoComponent } from './events-info/events-info.component';
 
@@ -12,6 +15,9 @@ import { EventsInfoComponent } from './events-info/events-info.component';
   imports: [
     CommonModule,
     MatDividerModule,
+    MatCardModule,
+    MatListModule,
+    MatGridListModule,
     EventsPageRoutingModule,
     SharedModule
   ]

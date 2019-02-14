e176ac1929e42813238dfc6967b62206d4e7ff7f
Qarun Qadir Bissoondial <qarun80@hotmail.com>
2019-02-14 15:48:54 -0400

    - Deleted project dialog component as it is no longer needed - Re-designed project card for both admin and user side

--- src/app/shared-project/project-card/project-card.component.ts
+++ src/app/shared-project/project-card/project-card.component.ts
@@ -1,7 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {ProjectInfo} from '../../../model';
-import {MatDialog} from '@angular/material/dialog';
-import {ProjectDialogComponent} from '../project-dialog/project-dialog.component';
 
 @Component({
   selector: 'app-project-card',
@@ -15,20 +13,9 @@ export class ProjectCardComponent implements OnInit {
 
   @Input() project: ProjectInfo;
   @Input() admin: boolean;
-  constructor(public dialog: MatDialog) { }
+  constructor() { }
 
   ngOnInit() {
-  }
-
-  openDialog(): void {
-    this.dialog.open(ProjectDialogComponent, {
-      data: {
-        project: this.project,
-        admin: this.admin
-      }
-    });
-
-    if (window.innerWidth < 768) { (<HTMLElement>document.querySelector('.mat-dialog-container')).style.height = '75vh'; }
   }
 
 }
--- src/app/shared-project/shared-project.module.ts
+++ src/app/shared-project/shared-project.module.ts
@@ -3,26 +3,21 @@ import { CommonModule } from '@angular/common';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatListModule} from '@angular/material/list';
-import {ProjectDialogComponent} from './project-dialog/project-dialog.component';
 import {ProjectCardComponent} from './project-card/project-card.component';
-import {MatDialogModule} from '@angular/material';
 
 @NgModule({
   declarations: [
     ProjectCardComponent,
-    ProjectDialogComponent,
   ],
   exports: [
     ProjectCardComponent,
-    ProjectDialogComponent
   ],
   imports: [
     CommonModule,
     MatCardModule,
-    MatDialogModule,
     MatListModule,
     MatButtonModule
   ],
-  entryComponents: [ProjectDialogComponent]
+  entryComponents: []
 })
 export class SharedProjectModule { }
